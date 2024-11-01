fa8798eefc82667f9d4d93b82531f940082d78ff
Andreas Højgaard-Gammeltoft <afhoejgaard@gmail.com>
2024-11-01 23:06:29 +0100

    Rename parse function to parse_person and enhance key handling for improved data parsing

--- parse_person.py
+++ parse_person.py
@@ -2,13 +2,18 @@ from typing import Literal, cast
 from type_definitions import Person
 
 
-def parse(person: Person) -> Person:
+def parse_person(person: Person) -> Person:
     for key, value in person.items():
         value = cast(str, value)
-        key = cast(Literal['Age', 'Income', 'Travel_Frequency', 'Vacation_Budget', 'Proximity_to_Mountains', 'Proximity_to_Beaches', 'Pets', 'Environmental_Concerns'], key)
+        key = cast(Literal['Age', 'Income', 'Travel_Frequency', 'Vacation_Budget', 'Proximity_to_Mountains', 'Proximity_to_Beaches', 'Pets', 'Environmental_Concerns', 'Preference'], key)
         if isinstance(key, str):
             try:
-                person[key] = int(value)
+                if key == 'Pets' or key == 'Environmental_Concerns':
+                    person[key] = bool(value == '1')
+                elif key == 'Preference':
+                    person[key] = 'beaches' if value == '0' else 'mountains'
+                else:
+                    person[key] = int(value)
             except:
                 continue
     
